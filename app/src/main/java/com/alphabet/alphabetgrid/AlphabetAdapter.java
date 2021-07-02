fbd99bf22024be44e9d60c4dc478c8d7ec0de99d
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-07-02 11:39:09 +0300

    add all changes

---
+++ app/src/main/java/com/alphabet/alphabetgrid/AlphabetAdapter.java
@@ -0,0 +1,8 @@
+package com.alphabet.alphabetgrid;
+
+import android.widget.ListAdapter;
+
+public class AlphabetAdapter implements ListAdapter {
+    public AlphabetAdapter(MainActivity mainActivity, String[] letters) {
+    }
+}
